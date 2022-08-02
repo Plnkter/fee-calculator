cc3b06d9a246c270f9acb8bb68c7ee4a3315d082
Charlotte Kam <>
2022-08-02 11:30:55 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useCallback, useMemo } from 'react';
 import './App.css';
+import { HelloButton } from './components/button';
+import RangeSlider from './components/range-slider';
 
 function App() {
+  const [parentVal, setParentVal] = useState(10);
+
+      const sliderValueChanged = useCallback(val => {
+        console.log("NEW VALUE", val);
+        setParentVal(val);
+      });
+
+      const sliderProps = useMemo(
+        () => ({
+          min: 0,
+          max: 100,
+          value: parentVal,
+          step: 2,
+          label: "This is a reusable slider",
+          onChange: e => sliderValueChanged(e)
+        }),
+        [parentVal]
+      );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloButton name={"Charlotte"}></HelloButton>
+      <h1>PARENT VALUE: {parentVal}</h1>
+      <RangeSlider {...sliderProps} classes="additional-css-classes" />
     </div>
   );
 }
